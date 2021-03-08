7b88411bddbd39c0226df1e86daf914e18cf24bf
surendhar22 <surendhars@hotmail.com>
2021-03-08 22:01:50 +0800

    insert twitter data to bigquery

--- twitter/twitterHistorical.py
+++ twitter/twitterHistorical.py
@@ -4,6 +4,7 @@ from tweepy import Cursor
 import pymongo
 from datetime import datetime, timedelta
 import logging
+from google.cloud import bigquery
 
 import sys
 
@@ -15,24 +16,41 @@ class IngestHistoricalData(object):
     mydb = myclient["TwitterDB"]
     mycol = mydb["tweetsData"]
 
+    bigquery_client = bigquery.Client()
+    table_id = "bead-project.twitter_dataset.tweets_dataset"
+
+
+
     search_word = '#bitcoin OR #btn OR #blockchain OR #cryptocurrency OR #cryptocurrencies OR #crypto OR #satoshi -filter:retweets'
 
     def get_tweets(self,api,start_date,end_date):
         tweets = Cursor(api.search, q=self.search_word, lang='en', since=start_date, until=end_date).items(10)
         return tweets
 
+    def insert_to_bigquery(self,rows_to_insert):
+        errors = self.bigquery_client.insert_rows_json(self.table_id, rows_to_insert)
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
+
     def save_tweets(self,tweets):
+        count = 0
+        rows_to_insert = []
         for tweet in tweets:
 
             json_data = {
-                'tweet_id': tweet._json.get('id'),
-                'twitter_handle_name': tweet._json.get('user').get('screen_name'),
-                'twitter_handle_id': tweet._json.get('user').get('id'),
-                'twitter_datetime': tweet._json.get('created_at'),
-                'twitter_handle_desc': tweet._json.get('user').get('description')
+                u'tweet_id': tweet._json.get('id'),
+                u'twitter_handle_name': tweet._json.get('user').get('screen_name'),
+                u'twitter_handle_id': tweet._json.get('user').get('id'),
+                u'twitter_datetime': tweet._json.get('created_at'),
+                u'twitter_handle_desc': tweet._json.get('user').get('description')
             }
-            print('twee?',json_data)
-            self.mycol.insert_one(json_data)
+            rows_to_insert.append(json_data)
+            count = count + 1
+            if count % 1000 == 0:
+                self.insert_to_bigquery(rows_to_insert)
+                rows_to_insert = []
 
 
 if __name__ == "__main__":
