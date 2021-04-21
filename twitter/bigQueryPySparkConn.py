5f4cf46edf63cc1e2aea233374086a978b41ab12
surendhar22 <surendhars@hotmail.com>
2021-04-21 20:29:40 +0800

    bigQuerypySpark conn

--- twitter/bigQueryPySparkConn.py
+++ twitter/bigQueryPySparkConn.py
@@ -1,44 +1,45 @@
 from pyspark.sql import SparkSession
+import google.auth
 from google.cloud import bigquery
+
 from google.oauth2 import service_account
 #credentials = service_account.Credentials.from_service_account_file('C:/Users/suren/Documents/bead_project/bead-project-18e42f4e742d.json')
-credentials = service_account.Credentials.from_service_account_file('C:/Users/suren/Documents/bead_project/nice-forge-305606-0c1b603cf119.json')
+GOOGLE_APPLICATION_CREDENTIALS = 'C:/Users/suren/Documents/bead_project/direct-analog-308416-f082eab9c7fa.json'
+cred = google.auth.load_credentials_from_file(GOOGLE_APPLICATION_CREDENTIALS)
 
 
 
-project_id = 'nice-forge-305606'
-client = bigquery.Client(credentials= credentials,project=project_id)
+project_id = 'direct-analog-308416'
+client = bigquery.Client(credentials= cred[0],project=cred[1])
 
-QUERY = (
-    'SELECT DISTINCT(twitter_handle_id),twitter_handle_name FROM `nice-forge-305606.twitter_dataset.tweets_data` ')
+print(client)
 
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+spark = SparkSession \
+  .builder \
+  .appName('spark-bigquery-demo') \
+  .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.11:0.15.1-beta') \
+  .getOrCreate()
+
+bucket = "tweets-bucker"
+spark.conf.set('temporaryGcsBucket', bucket)
 
 
+table =  "direct-analog-308416.project_data.tweets_data"
+df = spark.read.format("bigquery") \
+    .option("credentialsFile",GOOGLE_APPLICATION_CREDENTIALS)\
+    .option('project','direct-analog-308416')\
+    .option('parentProject','direct-analog-308416')\
+    .option("dataset", "project_data")\
+    .option('table', 'tweets_data') \
+    .load()
 
-for row in rows:
-    print(row.twitter_handle_id)
+print('ummm')
+df.createOrReplaceTempView('df')
 
-
-#
-# print(client)
-#
-# spark = SparkSession \
-#   .builder \
-#   .appName('spark-bigquery-demo') \
-#   .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.11:0.15.1-beta') \
-#   .getOrCreate()
-#
-# # bucket = "nice-forge-305606"
-# # spark.conf.set('temporaryGcsBucket', bucket)
-#
-# table = "nice-forge-305606:twitter_dataset.tweets_data"
-# df = spark.read.format("bigquery") \
-#   .option('table', table) \
-#   .load()
-#
-# print(df)
+df2 = spark.sql(
+    'SELECT * FROM df')
+print('mmmu')
+print(df2.show())
 
 # words = spark.read.format('bigquery') \
 #   .option('table', 'twitter_dataset.tweets_data') \
