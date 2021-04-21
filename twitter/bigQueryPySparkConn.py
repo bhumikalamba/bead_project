376a5e52b0476c81cd91d0c6c4fd8d20f3126fc3
surendhar22 <surendhars@hotmail.com>
2021-04-22 00:21:55 +0800

    bigQuerypySpark conn

--- twitter/bigQueryPySparkConn.py
+++ twitter/bigQueryPySparkConn.py
@@ -4,7 +4,7 @@ from google.cloud import bigquery
 
 from google.oauth2 import service_account
 #credentials = service_account.Credentials.from_service_account_file('C:/Users/suren/Documents/bead_project/bead-project-18e42f4e742d.json')
-GOOGLE_APPLICATION_CREDENTIALS = 'C:/Users/suren/Documents/bead_project/direct-analog-308416-f082eab9c7fa.json'
+GOOGLE_APPLICATION_CREDENTIALS = './direct-analog-308416-f082eab9c7fa.json'
 cred = google.auth.load_credentials_from_file(GOOGLE_APPLICATION_CREDENTIALS)
 
 
@@ -17,29 +17,27 @@ print(client)
 spark = SparkSession \
   .builder \
   .appName('spark-bigquery-demo') \
-  .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.11:0.15.1-beta') \
+  .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.15.1-beta') \
   .getOrCreate()
 
 bucket = "tweets-bucker"
 spark.conf.set('temporaryGcsBucket', bucket)
 
-
+print("heyeeeeee")
 table =  "direct-analog-308416.project_data.tweets_data"
 df = spark.read.format("bigquery") \
-    .option("credentialsFile",GOOGLE_APPLICATION_CREDENTIALS)\
-    .option('project','direct-analog-308416')\
-    .option('parentProject','direct-analog-308416')\
-    .option("dataset", "project_data")\
-    .option('table', 'tweets_data') \
+    .option('credentialsFile',GOOGLE_APPLICATION_CREDENTIALS)\
+    .option('table', 'direct-analog-308416.project_data.twitter_data_ingest') \
     .load()
 
-print('ummm')
+
+print(df)
 df.createOrReplaceTempView('df')
 
 df2 = spark.sql(
-    'SELECT * FROM df')
-print('mmmu')
-print(df2.show())
+    'SELECT tweet_id FROM df WHERE followers_count < 50')
+
+df2.show()
 
 # words = spark.read.format('bigquery') \
 #   .option('table', 'twitter_dataset.tweets_data') \
