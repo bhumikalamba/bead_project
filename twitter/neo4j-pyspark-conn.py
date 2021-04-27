6074e19b47fe7bcfe56776437ebbb972a9485a72
surendhar22 <surendhars@hotmail.com>
2021-04-28 00:46:32 +0800

    neo4j-pyspark

--- twitter/neo4j-pyspark-conn.py
+++ twitter/neo4j-pyspark-conn.py
@@ -21,39 +21,57 @@ this pulls the executes the query and pulls the data
 
 .option("query", "MATCH (n:User) WHERE n.screen_name CONTAINS '' RETURN n.screen_name,n.id_str,n.description,n.followers_count")\
 '''
-df = spark.read.format("org.neo4j.spark.DataSource") \
-  .option("url", "bolt://localhost:7687") \
+src_tgt_df = spark.read.format("org.neo4j.spark.DataSource") \
+  .option("url", "bolt://34.87.46.194:7687") \
   .option("authentication.type", "basic") \
   .option("authentication.basic.username", "neo4j") \
-  .option("authentication.basic.password", "password") \
+  .option("authentication.basic.password", "$martBEAD&") \
   .option("relationship","FOLLOWS") \
   .option("relationship.nodes.map", "false")\
   .option("relationship.source.labels", "User")\
   .option("relationship.target.labels", "User")\
   .load()
 
+#df.select("`source.id_str`").show(300,truncate=False)
+#df.select('`<source>`.`id_str`').show(300,truncate=False)
+#df.select('`<target>`.`id_str`').show(5,truncate=False)
+#df.select('`<target>`.`id_str`').show(20,truncate=False)
+#df.where('`<target>`.`screen_name` == ""').show()
+# print('HEYY')
+#edges = df.select("`<rel.type>`","`source.id_str`","`target.id_str`").selectExpr("`<rel.type>` as relationship", "`source.id_str` as src", "`target.id_str` as dst")
 
-edges = df.select("`<rel.type>`","`source.id_str`","`target.id_str`").selectExpr("`<rel.type>` as relationship", "`source.id_str` as src", "`target.id_str` as dst")
 
-
-vertex = spark.read.format("org.neo4j.spark.DataSource") \
-  .option("url", "bolt://localhost:7687") \
+btc_tweeted_users = spark.read.format("org.neo4j.spark.DataSource") \
+  .option("url", "bolt://34.87.46.194:7687") \
   .option("authentication.type", "basic") \
   .option("authentication.basic.username", "neo4j") \
-  .option("authentication.basic.password", "password") \
+  .option("authentication.basic.password", "$martBEAD&") \
   .option("query",
           "MATCH (n:User) WHERE n.screen_name CONTAINS '' RETURN n.screen_name as screen_name,n.id_str as id,n.description as description,n.followers_count as followers_count")\
   .load()
 
+
+
+
+btc_tweeted_users_relations = btc_tweeted_users.join(src_tgt_df, btc_tweeted_users.id == src_tgt_df['`source.id_str`'], how='left').na.drop(subset=["`<rel.type>`"])
+
+btc_tweeted_users_relations.show()
+
+vertex = btc_tweeted_users_relations.select("screen_name","id","description","followers_count")
+edges = btc_tweeted_users_relations.select("id", "`target.id_str`","`<rel.type>`").selectExpr("`<rel.type>` as relationship", "id as src", "`target.id_str` as dst")
+
+print('Vertex comiong up')
 vertex.show()
 
+print('Edges coming up')
 edges.show()
+
 from graphframes import GraphFrame
-
+#
 g = GraphFrame(vertex,edges)
-
+#
 g.inDegrees.show()
 
-results = g.pageRank(resetProbability=0.01, maxIter=20)
+results = g.pageRank(resetProbability=0.01, maxIter=2)
 results.vertices.select("id", "pagerank").show()
 #   .option("query", "MATCH (n:User) WHERE n.screen_name CONTAINS '' RETURN n.screen_name,n.id_str,n.description,n.followers_count")\
